1175bcca7a3f7c4d16f1c244b83b6b28ce82aa69
Vedant Chainani <vedantchainani1084@gmail.com>
2026-03-24 23:31:32 +0530

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,23 @@ import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
 
-const config = defineConfig({
+export default defineConfig({
   ssr: {
-    external: ['@takumi-rs/image-response','@takumi-rs/core'],
+    external: ['@takumi-rs/image-response', '@takumi-rs/core'],
   },
   optimizeDeps: {
-    include: ['@takumi-rs/image-response','@takumi-rs/core'],
+    exclude: ['@takumi-rs/image-response', '@takumi-rs/core'],
   },
   plugins: [
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
-    nitro({
-      traceDeps: ['@takumi-rs/core'],
-    }),
+    nitro(),
     viteReact(),
   ],
 })
-
-export default config
